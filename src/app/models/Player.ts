732a3066d8604c59f5bb004c1d10c8d433216878
Jody Wells <Jody.Wells@vu.com>
2019-06-27 12:23:45 -0500

    Just keeping on working

--- src/app/models/Player.ts
+++ src/app/models/Player.ts
@@ -1,4 +1,7 @@
+import { AbstractFormGroupDirective } from "@angular/forms";
+
 export class Player {
+    public PlayerId: Guid,
     public FirstName: string;
     public LastName: string;
     public BggUserName?: string;
