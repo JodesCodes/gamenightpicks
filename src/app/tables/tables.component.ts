70f4b26d2fe49b13969edd5d7ebd529476269867
Jody Wells <jodywells406@gmail.com>
2020-04-03 16:52:18 -0500

    Running on Chromebook

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import {Table } from '../models/table';
-import { TableStatus } from '../models/tablestatus';
+import { TableStatus } from '../models/TableStatus';
 
  const tablesData: Table[] = [
-   {Title: "Evolution", MaxPlayers: 6, PlayTime: "60 Minutes", Status: TableStatus.Open, Players: [{FirstName: "Jody", LastName: "Wells", BggUserName: "Jodes"},{FirstName: "Skye", LastName: "Griffin", BggUserName: ""},{FirstName: "Luke", LastName: "Baker", BggUserName: ""},{FirstName: "Mal", LastName: "Ensor", BggUserName: ""}]},
-   {Title: "Underwater Cities", MaxPlayers: 4, PlayTime: "80-150 Minutes", Status: TableStatus.Full, Players: [{FirstName: "Adam", LastName: "Wickham", BggUserName: ""},{FirstName: "John", LastName: "Meyer", BggUserName: "masteroftheboards"},{FirstName: "David", LastName: "Hansen", BggUserName: ""},{FirstName: "Ben", LastName: "Schirmer", BggUserName: ""}]},
-   {Title: "Quodd Heroes", MaxPlayers: 5, PlayTime: "30-90 Minutes", Status: TableStatus.Open, Players: [{FirstName: "Kevin", LastName: "Stoll", BggUserName: ""},{FirstName: "Brett", LastName: "Gilbert", BggUserName: ""}]},
-
+   {Title: "Evolution", MaxPlayers: 6, PlayTime: "60 Minutes", Status: TableStatus.Open, Players: [{UserName: "JodesCodes", FirstName: "Jody", LastName: "Wells", BggUserName: "Jodes"},{UserName: "SkyEE", FirstName: "Skye", LastName: "Griffin", BggUserName: ""},{UserName: "akaBaker", FirstName: "Luke", LastName: "Baker", BggUserName: ""},{UserName: "ChaoticNeutral", FirstName: "Mal", LastName: "Ensor", BggUserName: ""}]},
+   {Title: "Underwater Cities", MaxPlayers: 4, PlayTime: "80-150 Minutes", Status: TableStatus.Full, Players: [{UserName: "WickDawg", FirstName: "Adam", LastName: "Wickham", BggUserName: ""},{UserName: "MasteroftheBoards", FirstName: "John", LastName: "Meyer", BggUserName: "masteroftheboards"},{UserName: "styles", FirstName: "David", LastName: "Hansen", BggUserName: ""},{UserName: "bschirm", FirstName: "Ben", LastName: "Schirmer", BggUserName: ""}]},
+   {Title: "Quodd Heroes", MaxPlayers: 5, PlayTime: "30-90 Minutes", Status: TableStatus.Open, Players: [{UserName: "kstoll1087", FirstName: "Kevin", LastName: "Stoll", BggUserName: ""},{UserName: "MaturinYDomonova", FirstName: "Brett", LastName: "Gilbert", BggUserName: ""}]},
  ]
 
 @Component({
